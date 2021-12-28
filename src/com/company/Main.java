2d840cbc5e3d37e0aa4638e2cdfb4451f060ce80
gopala01 <bhamidipatig@gmail.com>
2021-12-28 10:19:13 +0000

    Finished attempt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,62 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String dash = "";
+        int num = inputInt("What is the max stars you want?");
+
+        num = checkNum(num);
+
+        printDashes(num, dash);
+    }
+
+    public static void printDashes(int num, String dash)
+    {
+        for (int i = 2; i <= num; i = i + 2) {
+            dash = getDashes(dash, 2);
+            print(dash);
+        }
+    }
+
+
+    public static int checkNum(int num)
+    {
+        while (num > 100 || num < 0)
+        {
+            num = inputInt("Choose between 0 and 100");
+        }
+        if (num%2 == 0 )
+        {
+            return num;
+        }
+        else
+        {
+            return num+1;
+        }
+    }
+    public static String getDashes(String d, int i)
+    {
+        for (int j = 0; j < i; j++) {
+            d = d + "*";
+        }
+
+        return d;
+    }
+
+    public static void print(String m)
+    {
+        System.out.println(m);
+    }
+
+    public static int inputInt(String m) {
+        int answer;
+        Scanner scanner = new Scanner(System.in);
+
+        print(m);
+        answer = scanner.nextInt();
+        return answer;
     }
 }
